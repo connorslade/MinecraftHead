8cc26a5652169d74847f461e85c888bdadfea714
Connor Slade <turtleslade@gmail.com>
2021-06-29 16:02:31 -0400

    🎂 Game Sales

--- src/async.js
+++ src/async.js
@@ -101,10 +101,25 @@ async function isServerBlocked(server) {
     });
 }
 
+/**
+ * Gets stats on minecraft sales
+ * @async
+ * @returns {Promise<Object>} {total, last24h, saleVelocityPerSeconds}
+ */
+async function stats() {
+    return new Promise(function(resolve, reject) {
+        let keys = ["item_sold_minecraft", "prepaid_card_redeemed_minecraft"]
+        common.post({metricKeys: keys}, 'api.mojang.com', 443, '/orders/statistics', function (data) {
+            resolve({total: data.total, last24h: data.last24h, saleVelocityPerSeconds: data.saleVelocityPerSeconds})
+        });
+    })
+}
+
 module.exports = {
     nameToUuid,
     uuidToName,
     getSkin,
     statusCheck,
-    isServerBlocked
+    isServerBlocked,
+    stats
 }
--- src/common.js
+++ src/common.js
@@ -2,6 +2,7 @@ const crypto = require('crypto')
 const https = require('https')
 
 module.exports = {
+    // Get request
     get: function(uri, callback) {
         https.get(uri, (response) => {
             let todo = '';
@@ -16,11 +17,36 @@ module.exports = {
         });
     },
 
+    // Post Request
+    post: function(data, hostname, port, path, callback) {
+        data = JSON.stringify(data)
+        const options = {
+            hostname: hostname,
+            port: port,
+            path: path,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+            }
+        }
+        
+        const req = https.request(options, res => {
+            let todo = '';
+            res.on('data', d => todo += d)
+            res.on('end', () => callback(todo));
+        })
+        req.write(data)
+        req.end()
+    },
+
+    // Decode Base 64 encoded strings
     base64Decode: function (base64) {
         let buff = Buffer.from(base64, 'base64');
         return  buff.toString('utf-8');
     },
 
+    // Check if a url / ip is in the server Blocklist
     checkIfBlocked: function(url, blocked) {
         let shasum = crypto.createHash('sha1')
         shasum.update(url)
